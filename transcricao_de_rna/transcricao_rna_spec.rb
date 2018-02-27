227ea148097a1534501419e3baa3b4588a0b1267
SeixasDev <seixas.lc@gmail.com>
2018-02-27 18:45:05 -0300

    Adiciona Resolução da Atividade 2: Transcricao de RNA, pelo Aluno Lucas Seixas

---
+++ transcricao_de_rna/transcricao_rna_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'aaaa-s/nome'
+
+describe Complemento, '.do_dna' do
+  context 'sequências válidas' do
+    it { expect(Complemento.do_dna('C')).to eq 'G' }
+    it { expect(Complemento.do_dna('G')).to eq 'C' }
+    it { expect(Complemento.do_dna('T')).to eq 'A' }
+    it { expect(Complemento.do_dna('A')).to eq 'U' }
+    it { expect(Complemento.do_dna('ACGTGGTCTTAA')).to eq 'UGCACCAGAAUU' }
+  end
+  context 'sequências inválidas' do
+    it { expect(Complemento.do_dna('U')).to eq '' }
+    it { expect(Complemento.do_dna('XXX')).to eq '' }
+    it { expect(Complemento.do_dna('ACGTXXXCTTAA')).to eq '' }
+  end
+end
