ce93bb2ea3a44c76c7a72235dc1b05adf76c3da4
Élisson Michael <elissonmichael@yahoo.com.br>
2018-03-17 15:49:18 -0300

    Adiciona Rspec e Implementação Parcial de #19

---
+++ taverna_medieval/taverna_medieval_spec.rb
@@ -0,0 +1,84 @@
+require_relative 'taverna_medieval'
+
+describe TavernaMedieval, '#atualizar_qualidade' do
+  context 'Quando é um Item Comum' do
+    it 'Não muda o nome do item' do
+      items = [Item.new('Espada Longa +1', 10, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].nome).to eq 'Espada Longa +1'
+    end
+    it 'Decrementa vence_em em 1 com o tempo' do
+      items = [Item.new('Espada Longa +1', 10, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].vence_em).to eq 9
+    end
+    it 'Decrementa a qualidade em 1 com o tempo' do
+      items = [Item.new('Espada Longa +1', 10, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 19
+    end
+    it 'Não deixa a qualidade negativa com o tempo' do
+      items = [Item.new('Armadura de Couro', 10, 0)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 0
+    end
+    it 'Decrementa vence_em em 2 com o tempo em itens vencidos' do
+      items = [Item.new('Armadura de Couro', 0, 10)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 8
+    end
+  end
+  context "Quando o Item é um 'Vinho'" do
+    it 'Incrementa a qualidade em 1 com o tempo' do
+      items = [Item.new('Vinho', 2, 0)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 1
+    end
+    it 'Não incrementa a qualidade acima do máximo (50)' do
+      items = [Item.new('Vinho', 2, 50)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 50
+    end
+  end
+  context "Quando o Item é um 'Lendário'" do
+    it 'Não decrementa a qualidade com o tempo' do
+      items = [Item.new('Lendário Martelo de Thor', 0, 80)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 80
+    end
+    it 'Não decrementa o prazo de vencimento com o tempo' do
+      items = [Item.new('Lendário Martelo de Thor', 1, 80)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].vence_em).to eq 1
+    end
+  end
+  context "Quando o Item é um 'Ingresso'" do
+    it 'Incrementa a qualidade em 1 com o tempo' do
+      items = [Item.new('Ingresso para uma Orquestra', 15, 48)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 49
+    end
+    it 'Incrementa a qualidade em 2 quando vencer_em é menor ou igual a 10' do
+      items = [Item.new('Ingresso para uma Orquestra', 10, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 22
+    end
+    it 'Incrementa a qualidade em 3 quando vencer_em é menor ou igual a 5' do
+      items = [Item.new('Ingresso para uma Orquestra', 5, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 23
+    end
+    it 'Zera a qualidade quando vence' do
+      items = [Item.new('Ingresso para uma Orquestra', 0, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 0
+    end
+  end
+  context "Quando o Item é uma 'Conjuração'" do
+    it 'Decrementa a qualidade em 2 com o tempo' do
+      items = [Item.new('Arco Conjurado', 3, 6)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 4
+    end
+  end
+end
