05f49a837aae6bfd15a083bc4613ceedbf9053c2
Élisson Michael <elissonmichael@yahoo.com.br>
2018-03-18 18:16:32 -0300

    Adiciona Teste Rspec do Último Requisito de #6

--- carrinho_de_compras/carrinho_compras_spec.rb
+++ carrinho_de_compras/carrinho_compras_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'aaaa-s/nome'
+require_relative '2018-1/elisson'
 
 describe Carrinho do
   let(:produto)  { Produto.new('Mass Effect 3', 50) }
@@ -51,6 +51,17 @@ describe Carrinho do
       carrinho.incluir(bioshock)
       carrinho.alterar_quantidade(bioshock, 2)
       expect(carrinho.total).to eq 130
+    end
+  end
+  context 'Relatório para Impressão' do
+    it 'Retorna um Relatório com a Data de Inclusão de Cada Produto' do
+      carrinho.incluir(produto)
+      bioshock = Produto.new('BioShock 2', 40)
+      carrinho.incluir(bioshock)
+      carrinho.alterar_quantidade(bioshock, 2)
+      data = Time.now.strftime('%d/%m/%y %I:%M%p')
+      relatorio = "Mass Effect 3 adicionado em #{data}, BioShock 2 adicionado em #{data}"
+      expect(carrinho.relatorio).to eq relatorio
     end
   end
 end
