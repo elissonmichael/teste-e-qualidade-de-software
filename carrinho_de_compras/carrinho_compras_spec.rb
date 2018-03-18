73bd41f18ae0d84f82a28dd89c1043614ecbeb7b
Élisson Michael <elissonmichael@yahoo.com.br>
2018-03-18 18:24:52 -0300

    Refactora Rspec de #6 para DRY

--- carrinho_de_compras/carrinho_compras_spec.rb
+++ carrinho_de_compras/carrinho_compras_spec.rb
@@ -1,8 +1,9 @@
-require_relative '2018-1/elisson'
+require_relative 'aaaa-s/nome'
 
 describe Carrinho do
-  let(:produto)  { Produto.new('Mass Effect 3', 50) }
-  let(:carrinho) { Carrinho.new }
+  let(:produto)       { Produto.new('Mass Effect 3', 50) }
+  let(:carrinho)      { Carrinho.new }
+  let(:outro_produto) { Produto.new('BioShock 2', 40) }
 
   def adiciona_10_produtos_iguais
     10.times { carrinho.incluir(produto) }
@@ -47,18 +48,15 @@ describe Carrinho do
   context 'Cálculo de Preço' do
     it 'Calcula Valor Total dos seus Produtos' do
       carrinho.incluir(produto)
-      bioshock = Produto.new('BioShock 2', 40)
-      carrinho.incluir(bioshock)
-      carrinho.alterar_quantidade(bioshock, 2)
+      carrinho.incluir(outro_produto)
+      carrinho.alterar_quantidade(outro_produto, 2)
       expect(carrinho.total).to eq 130
     end
   end
   context 'Relatório para Impressão' do
     it 'Retorna um Relatório com a Data de Inclusão de Cada Produto' do
       carrinho.incluir(produto)
-      bioshock = Produto.new('BioShock 2', 40)
-      carrinho.incluir(bioshock)
-      carrinho.alterar_quantidade(bioshock, 2)
+      carrinho.incluir(outro_produto)
       data = Time.now.strftime('%d/%m/%y %I:%M%p')
       relatorio = "Mass Effect 3 adicionado em #{data}, BioShock 2 adicionado em #{data}"
       expect(carrinho.relatorio).to eq relatorio
