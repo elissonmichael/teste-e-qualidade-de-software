a01a94181ffa17c074627630663f1a5e2cb7d0fb
Élisson Michael <elissonmichael@yahoo.com.br>
2018-03-24 15:38:11 -0300

    Melhora os Testes Rspec de #19

--- taverna_medieval/taverna_medieval_spec.rb
+++ taverna_medieval/taverna_medieval_spec.rb
@@ -1,26 +1,49 @@
 require_relative 'taverna_medieval'
 
 describe TavernaMedieval, '#atualizar_qualidade' do
+
+  shared_examples_for 'Vencer_em de Item Padrão' do |nome_do_item|
+    it 'Decrementa vence_em em 1 com o tempo' do
+      items = [Item.new(nome_do_item, 10, 20)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].vence_em).to eq 9
+    end
+  end
+
+  shared_examples_for 'Qualidade de Item Padrão' do |nome_do_item|
+    it 'Não deixa a qualidade negativa com o tempo' do
+      items = [Item.new(nome_do_item, 20, 0)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 0
+    end
+  end
+
+  shared_examples_for 'Qualidade de Vinho' do |nome_do_item|
+    it 'Incrementa a qualidade em 1 com o tempo' do
+      items = [Item.new(nome_do_item, 20, 0)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 1
+    end
+    it 'Não incrementa a qualidade acima do máximo (50)' do
+      items = [Item.new(nome_do_item, 20, 50)]
+      TavernaMedieval.new(items).atualizar_qualidade
+      expect(items[0].qualidade).to eq 50
+    end
+  end
+
   context 'Quando é um Item Comum' do
+    it_behaves_like 'Vencer_em de Item Padrão', 'Espada Longa +1'
+    it_behaves_like 'Qualidade de Item Padrão', 'Espada Longa +1'
+
     it 'Não muda o nome do item' do
       items = [Item.new('Espada Longa +1', 10, 20)]
       TavernaMedieval.new(items).atualizar_qualidade
       expect(items[0].nome).to eq 'Espada Longa +1'
     end
-    it 'Decrementa vence_em em 1 com o tempo' do
-      items = [Item.new('Espada Longa +1', 10, 20)]
-      TavernaMedieval.new(items).atualizar_qualidade
-      expect(items[0].vence_em).to eq 9
-    end
     it 'Decrementa a qualidade em 1 com o tempo' do
       items = [Item.new('Espada Longa +1', 10, 20)]
       TavernaMedieval.new(items).atualizar_qualidade
       expect(items[0].qualidade).to eq 19
-    end
-    it 'Não deixa a qualidade negativa com o tempo' do
-      items = [Item.new('Armadura de Couro', 10, 0)]
-      TavernaMedieval.new(items).atualizar_qualidade
-      expect(items[0].qualidade).to eq 0
     end
     it 'Decrementa qualidade em 2 com o tempo em itens vencidos' do
       items = [Item.new('Armadura de Couro', 0, 10)]
@@ -28,18 +51,18 @@ describe TavernaMedieval, '#atualizar_qualidade' do
       expect(items[0].qualidade).to eq 8
     end
   end
+
   context "Quando o Item é um 'Vinho'" do
-    it 'Incrementa a qualidade em 1 com o tempo' do
-      items = [Item.new('Vinho', 2, 0)]
+    it_behaves_like 'Vencer_em de Item Padrão', 'Vinho'
+    it_behaves_like 'Qualidade de Vinho', 'Vinho'
+
+    it 'Incrementa a qualidade em 2 com o tempo quando vencido' do
+      items = [Item.new('Vinho', 0, 0)]
       TavernaMedieval.new(items).atualizar_qualidade
-      expect(items[0].qualidade).to eq 1
-    end
-    it 'Não incrementa a qualidade acima do máximo (50)' do
-      items = [Item.new('Vinho', 2, 50)]
-      TavernaMedieval.new(items).atualizar_qualidade
-      expect(items[0].qualidade).to eq 50
+      expect(items[0].qualidade).to eq 2
     end
   end
+
   context "Quando o Item é um 'Lendário'" do
     it 'Não decrementa a qualidade com o tempo' do
       items = [Item.new('Lendário Martelo de Thor', 0, 80)]
@@ -52,12 +75,11 @@ describe TavernaMedieval, '#atualizar_qualidade' do
       expect(items[0].vence_em).to eq 1
     end
   end
+
   context "Quando o Item é um 'Ingresso'" do
-    it 'Incrementa a qualidade em 1 com o tempo' do
-      items = [Item.new('Ingresso para uma Orquestra', 15, 48)]
-      TavernaMedieval.new(items).atualizar_qualidade
-      expect(items[0].qualidade).to eq 49
-    end
+    it_behaves_like 'Vencer_em de Item Padrão', 'Ingresso para uma Orquestra'
+    it_behaves_like 'Qualidade de Vinho', 'Ingresso para uma Orquestra'
+
     it 'Incrementa a qualidade em 2 quando vencer_em é menor ou igual a 10' do
       items = [Item.new('Ingresso para uma Orquestra', 10, 20)]
       TavernaMedieval.new(items).atualizar_qualidade
@@ -74,7 +96,11 @@ describe TavernaMedieval, '#atualizar_qualidade' do
       expect(items[0].qualidade).to eq 0
     end
   end
+
   context "Quando o Item é uma 'Conjuração'" do
+    it_behaves_like 'Vencer_em de Item Padrão', 'Arco Conjurado'
+    it_behaves_like 'Qualidade de Item Padrão', 'Arco Conjurado'
+
     it 'Decrementa a qualidade em 2 com o tempo' do
       items = [Item.new('Arco Conjurado', 3, 6)]
       TavernaMedieval.new(items).atualizar_qualidade
