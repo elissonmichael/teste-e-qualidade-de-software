ade6c7dba162ddc50c9614f0386fefff2025e759
João Pedro S. Rezende <jp_srezende@hotmail.com>
2018-11-22 13:38:08 -0200

    Resolve issue #47

---
+++ 2018-2/Joao_Pedro/arvore.rb
@@ -0,0 +1,28 @@
+class Arvore
+
+  attr_reader :idade, :altura, :frutas, :viva
+
+  def initialize
+    @idade = 0
+    @altura = 0
+    @frutas = 0
+  end
+
+  def viva?
+    @idade < 90
+  end
+
+  def passar_um_ano
+    @idade += 1
+    @altura += 0.5
+    @frutas = 0
+    if @idade >= 2 && viva?
+      @frutas = 10
+    end
+  end
+
+  def pegar_uma_fruta
+    @frutas -= 1 unless @frutas.eql?(0)
+  end
+  
+end
