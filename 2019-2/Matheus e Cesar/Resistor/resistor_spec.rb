3e0d36333022462fa07bb93e59eb2fd1dd670fcd
Matheus Lima <matheusldasilva20088@gmail.com>
2019-09-03 12:50:56 -0300

    separacao dos arquivos de testes e classe ref on issue #82

--- 2019-2/Matheus e Cesar/Resistor/resistor_spec.rb
+++ 2019-2/Matheus e Cesar/Resistor/resistor_spec.rb
@@ -1,24 +1,5 @@
-class Resistor
-   
-   
-    COLORS = %w(
-        preto
-        marrom
-        vermelho
-        laranja
-        amarelo
-        verde
-        azul
-        violeta
-        cinza
-        branco
-      )
-    
-    def valor(vetor)
-        vetor.map {|color| COLORS.index(color)}.join.to_i
-         end
-end
 
+require_relative 'resistor.rb'
 
 require 'minitest/autorun'
 
