d219acc58f6f44ea77a8e0fc16f2749f08ffca21
Vagner <ti.vagner@gmail.com>
2019-09-11 20:42:39 -0300

    Soluçao contagem de palavras ref on issue #7

---
+++ #7 contagem_palavras/2019-2/Vagner e Gabriel/contagem_palavras.rb
@@ -0,0 +1,14 @@
+class Frase
+  attr_accessor :frase
+  def initialize(frase)
+    @frase = frase
+  end
+
+  def conte_palavras()
+    hash = Hash.new(0)
+    @frase.downcase.gsub(/[^a-z0-9]/, ' ').split(' ').each do |palavra|
+    hash[palavra]+=1
+    end
+      return hash
+  end
+end
