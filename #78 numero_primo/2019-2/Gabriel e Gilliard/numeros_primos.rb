a6962e661ea18e4ed69fe9666d32c490c420f27c
gabriel04859 <42447528+gabriel04859@users.noreply.github.com>
2019-09-11 11:48:07 -0300

    Update numeros_primos.rb

--- #78 numero_primo/2019-2/Gabriel e Gilliard/numeros_primos.rb
+++ #78 numero_primo/2019-2/Gabriel e Gilliard/numeros_primos.rb
@@ -1,7 +1,9 @@
-module Numero_primo
-def self.primo?
-  (2..(self - 1)).each do |numero|
-    return false if self % numero == 0
-  end
-  true
+class Integer
+    def primo?
+         if self > 1 && (self % 1) == 0 && (self % self) == 0 && (2..self - 1).none? { |numero| (self % numero) == 0 }
+           return true
+         else
+           return false
+         end
+       end
 end
